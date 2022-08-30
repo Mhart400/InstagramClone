8b6aed5ac976555ee342ff35a8d52702d90e669f
Mhart400 <mhart400@gmail.com>
2022-08-29 21:00:15 -0400

    mm

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Wolrd!</Text>
+      <Text>Hello Wolrd!!</Text>
       <StatusBar style="auto" />
     </View>
   );
