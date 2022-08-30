e6f631907107be73ed267afcd896358d1b248e84
Mhart400 <mhart400@gmail.com>
2022-08-29 21:07:34 -0400

    smallChange

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Wolrd!!</Text>
+      <Text>Hello Wolrds!!</Text>
       <StatusBar style="auto" />
     </View>
   );
