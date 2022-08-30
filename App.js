f2ab0b408be71e511940d802f5f2d5406c9a8052
Mhart400 <mhart400@gmail.com>
2022-08-30 13:42:51 -0400

    update

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import AuthLanding from "./src/Components/auth/AuthLanding";
+import Register from "./src/Components/auth/Register";
+import Login from "./src/Components/auth/Login";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/database";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKSA5nN9tkCNL6-oAgAsT4GDyww-b8DGQ",
+  authDomain: "instagramclone-42c2c.firebaseapp.com",
+  projectId: "instagramclone-42c2c",
+  storageBucket: "instagramclone-42c2c.appspot.com",
+  messagingSenderId: "138511774575",
+  appId: "1:138511774575:web:7cf1fe85725c998f596e9a",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
 
 export default function App() {
+  const [loaded, setLoaded] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    firebase.auth.OnAuthStateChanged((user) => {
+      if (!user) {
+        setLoggedIn(false);
+        setLoaded(false);
+      } else {
+        setLoaded(true);
+        setLoggedIn(true);
+      }
+    });
+  }, []);
+
+  if (!loaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hello Wolrds!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen
+          name="Landing"
+          component={AuthLanding}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Stack = createNativeStackNavigator();
